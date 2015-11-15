ad1835a33088807094b7e0037bd0c013955fad4f
Ming Zhang <zhangming0305@hotmail.com>
2015-11-15 14:33:53 +0100

    add css template module

---
+++ eupral-custom-template-fix-style/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Eupral E-Shop Website Style Template',
+    'description': 'Custom Template Style Module for Eupral E-Shop',
+    'author': 'Ming Zhang',
+    'depends': ['website'],
+    'data': [
+        'view/snippets_css.xml'
+    ],
+    'application': True,
+}
