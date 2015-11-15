128a45b3747207fc2ff0906d0478deb4272d7c53
Ming Zhang <zhangming0305@hotmail.com>
2015-11-15 11:34:15 +0100

    add add-on for project

---
+++ website_tabs/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Tabs Building Block',
+    'category': 'Website',
+    'summary': 'Building Block that allows you to create tabbed content',
+    'version': '1.0',
+    'description': """
+Tabs Building Block
+======================================
+Building Block that allows you to create tabbed content
+        """,
+    'author': 'Nedas Zilinskas <nedas.zilinskas@gmail.com>',
+    'website': 'http://nedaszilinskas.com',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/snippet.xml'
+    ],
+    'installable': True
+}
